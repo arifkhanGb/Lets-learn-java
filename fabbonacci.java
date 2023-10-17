322818ebada23f4ba26acad9d845487dee9771ac
WalkingTree <arif90526@gmail.com>
2023-10-17 14:54:06 +0530

    fabbonacci series.

---
+++ fabbonacci.java
@@ -0,0 +1,26 @@
+package FirstPackage;
+
+import java.util.Scanner;
+
+public class fabbonacci {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a random number");
+		int userInput = sc.nextInt();
+		int a=0, b=1,c;
+		
+		System.out.print(a+" ");
+		System.out.print(b+" ");
+		
+		for(int i = 1; i<=userInput;i++)
+		{
+			c=a+b;
+			System.out.print(c +" ");
+			 a=b;
+			 b=c;
+		}
+
+	}
+
+}
