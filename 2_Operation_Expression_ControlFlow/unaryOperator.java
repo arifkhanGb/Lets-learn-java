caa4834443424e7d85e723e9a2f55152522658ee
Arif Khan <114013310+arifkhanGb@users.noreply.github.com>
2023-11-01 17:52:38 +0530

    Add files via upload

---
+++ 2_Operation_Expression_ControlFlow/unaryOperator.java
@@ -0,0 +1,16 @@
+package SecondPackage;
+
+public class unaryOperator {
+
+	public static void main(String[] args) {
+		int a =10;
+		int result = ++a;
+		System.out.println(result);//11
+		 result = a++;
+		 System.out.println(a);//12
+		System.out.println(result);//11
+		
+
+	}
+
+}
