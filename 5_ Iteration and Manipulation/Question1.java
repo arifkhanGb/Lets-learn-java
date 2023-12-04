f39efe270d2a4ef8503563115509f804c3878d76
Arif Khan <114013310+arifkhanGb@users.noreply.github.com>
2023-12-04 18:50:42 +0530

    Add files via upload

---
+++ 5_ Iteration and Manipulation/Question1.java
@@ -0,0 +1,21 @@
+package Day_11_12;
+
+import java.util.ArrayList;
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+       list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+//        System.out.println(list);
+
+        for(String e : list)
+        {
+            System.out.println(e);
+        }
+
+    }
+}
