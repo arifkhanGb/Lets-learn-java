7467108b49637a42394f0abbed5207855acfcb7e
Arif Khan <114013310+arifkhanGb@users.noreply.github.com>
2024-01-15 15:54:15 +0530

    Add files via upload

---
+++ File Input Output/Tester.java
@@ -0,0 +1,47 @@
+package Day_13_14;
+
+class Table{
+
+  public synchronized void printTable(int n){
+        for(int i = 1; i<=n; i++) {
+            System.out.println( i);
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        }
+    }
+}
+class MyThread1 extends Thread{
+    Table t;
+    MyThread1(Table t)
+    {
+        this.t = t;
+
+    }
+    public void run(){
+        t.printTable(5);
+    }
+
+}
+class MyThread2 extends Thread{
+    Table t;
+    MyThread2(Table t){
+        this.t = t;
+    }
+    public void run()
+    {
+        t.printTable(10);
+    }
+}
+public class Tester {
+    public static void main(String[] args) {
+        Table obj = new Table();
+        MyThread1 t1 = new MyThread1(obj);
+        MyThread2 t2 = new MyThread2(obj);
+        t1.start();
+        t2.start();
+    }
+}
